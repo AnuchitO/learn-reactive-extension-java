d18ff893019fed7f6bf719af0faaa15227b84bc3
Pead Nong <anuchit.prasertsang@gmail.com>
2017-03-11 14:18:14 +0700

    prepair data to build steam

--- src/main/java/com/gable/test/AppMain.java
+++ src/main/java/com/gable/test/AppMain.java
@@ -8,15 +8,14 @@ import rx.functions.Action1;
 import rx.functions.Func0;
 import rx.functions.Func1;
 
-import java.util.IntSummaryStatistics;
+import java.util.*;
 import java.util.concurrent.Callable;
 
 
 public class AppMain {
     public static void main(String[] args) {
-        MyRepository repository = new MyRepository();
-        repository.setValue("value1");
-
+//        MyRepository repository = new MyRepository();
+    
 //        repository.getValueObs()
 //                .map(s -> s.length())
 //                .map(value -> value * value)
@@ -55,34 +54,74 @@ public class AppMain {
     }
 }
 
-class MyRepository {
-    private String value;
-
-    public String getValue() {
-        return value;
+class EmployeeRepository {
+    public Observable<Employee> list() {
+        return Observable.just(
+            new Employee(1L, "user1"),
+            new Employee(2L, "user2"),
+            new Employee(3L, "user3")
+        );
     }
-//    Observable is on duty when has subscripber
-    public Observable<String> getValueObs() {
-//        return Observable.just(getValue());
+}
 
-//        return Observable.defer(new Func0<Observable<String>>() {
-//            @Override
-//            public Observable<String> call() {
-//                System.out.println("== defer ==");
-//                return  Observable.just(getValue());
-//            }
-//        });
+class AddressRepository {
+    private Map<Long, List<Address>> addresses = new HashMap<>();
 
-        return Observable.fromCallable(new Callable<String>() {
-            @Override
-            public String call() throws Exception {
-                return getValue();
-            }
-        });
+    public AddressRepository() {
+        addresses.put(1L, Arrays.asList(
+                new Address(1L, "34/432", "add1 of emp1"),
+                new Address(1L, "432/42", "add2 of emp1")));
+        addresses.put(2L, Arrays.asList(
+                new Address(2L, "34/67", "add1 of emp2")));
+        addresses.put(3L, Arrays.asList(
+                new Address(3L, "78/67", "add1 of emp3"),
+                new Address(3L, "79/67", "add2 of emp3"),
+                new Address(3L, "70/67", "add3 of emp3")));
+    }
+}
 
+class Employee {
+    private Long id;
+    private String name;
+
+    public Employee(Long id, String name) {
+        this.id = id;
+        this.name = name;
     }
 
-    public void setValue(String value) {
-        this.value = value;
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class Address {
+    private Long employeeId;
+    private String no;
+    private String road;
+
+    public Address(Long employeeId, String no, String road) {
+        this.employeeId = employeeId;
+        this.no = no;
+        this.road = road;
+    }
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+    public String getRoad() {
+        return road;
+    }
+
+    public void setRoad(String road) {
+        this.road = road;
     }
 }
